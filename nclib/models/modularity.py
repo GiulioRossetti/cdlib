869c4dc80df3bf8fa9440487d92eaed71f16bb3b
giuliorossetti <giulio.rossetti@gmail.com>
2019-01-04 13:58:06 +0100

    :new: Add community discovery algorithms

--- nclib/models/modularity.py
+++ nclib/models/modularity.py
@@ -30,21 +30,25 @@ def louvain(g, weight='weight', resolution=1., randomize=False):
     return coms_louvain
 
 
-def leiden(g):
+def leiden(g, initial_membership=None, weights=None):
     """
 
+    :param initial_membership:
+    :param weights:
     :param g:
     :return:
     """
 
     g = convert_graph_formats(g, ig.Graph)
 
-    part = leidenalg.find_partition(g, leidenalg.ModularityVertexPartition)
+    part = leidenalg.find_partition(g, leidenalg.ModularityVertexPartition,
+                                    initial_membership=initial_membership, weights=weights
+                                    )
     coms = [g.vs[x]['name'] for x in part]
     return coms
 
 
-def rb_pots(g):
+def rb_pots(g, initial_membership=None, weights=None, resolution_parameter=1):
     """
 
     Reichardt, J., & Bornholdt, S. (2006).
@@ -55,85 +59,111 @@ def rb_pots(g):
     Community Structure in Directed Networks.
     Physical Review Letters, 100(11), 118703. 10.1103/PhysRevLett.100.118703
 
+    :param initial_membership:
+    :param weights:
+    :param resolution_parameter:
     :param g:
     :return:
     """
 
     g = convert_graph_formats(g, ig.Graph)
 
-    part = leidenalg.find_partition(g, leidenalg.RBConfigurationVertexPartition)
+    part = leidenalg.find_partition(g, leidenalg.RBConfigurationVertexPartition,
+                                    resolution_parameter=resolution_parameter,
+                                    initial_membership=initial_membership, weights=weights)
     coms = [g.vs[x]['name'] for x in part]
     return coms
 
 
-def rber_pots(g):
+def rber_pots(g, initial_membership=None, weights=None, node_sizes=None, resolution_parameter=1):
     """
 
     Reichardt, J., & Bornholdt, S. (2006).
     Statistical mechanics of community detection.
     Physical Review E, 74(1), 016110. 10.1103/PhysRevE.74.016110
 
+    :param initial_membership:
+    :param weights:
+    :param node_sizes:
+    :param resolution_parameter:
     :param g:
     :return:
     """
 
     g = convert_graph_formats(g, ig.Graph)
 
-    part = leidenalg.find_partition(g, leidenalg.RBERVertexPartition)
+    part = leidenalg.find_partition(g, leidenalg.RBERVertexPartition,
+                                    resolution_parameter=resolution_parameter,
+                                    initial_membership=initial_membership, weights=weights,
+                                    node_sizes=node_sizes,
+                                    )
     coms = [g.vs[x]['name'] for x in part]
     return coms
 
 
-def cpm(g):
+def cpm(g, initial_membership=None, weights=None, node_sizes=None, resolution_parameter=1):
     """
 
     Traag, V. A., Van Dooren, P., & Nesterov, Y. (2011).
     Narrow scope for resolution-limit-free community detection.
     Physical Review E, 84(1), 016114. 10.1103/PhysRevE.84.016114
 
+    :param initial_membership:
+    :param weights:
+    :param node_sizes:
+    :param resolution_parameter:
     :param g:
     :return:
     """
 
     g = convert_graph_formats(g, ig.Graph)
 
-    part = leidenalg.find_partition(g, leidenalg.CPMVertexPartition)
+    part = leidenalg.find_partition(g, leidenalg.CPMVertexPartition,
+                                    resolution_parameter=resolution_parameter, initial_membership=initial_membership,
+                                    weights=weights, node_sizes=node_sizes,)
     coms = [g.vs[x]['name'] for x in part]
     return coms
 
 
-def significance_communities(g):
+def significance_communities(g,  initial_membership=None, node_sizes=None):
     """
 
     Traag, V. A., Krings, G., & Van Dooren, P. (2013).
     Significant scales in community structure.
     Scientific Reports, 3, 2930. 10.1038/srep02930
 
+    :param initial_membership:
+    :param node_sizes:
     :param g:
     :return:
     """
 
     g = convert_graph_formats(g, ig.Graph)
 
-    part = leidenalg.find_partition(g, leidenalg.SignificanceVertexPartition)
+    part = leidenalg.find_partition(g, leidenalg.SignificanceVertexPartition, initial_membership=initial_membership,
+                                    node_sizes=node_sizes)
     coms = [g.vs[x]['name'] for x in part]
     return coms
 
 
-def surprise_communities(g):
+def surprise_communities(g, initial_membership=None, weights=None, node_sizes=None):
     """
 
     Traag, V. A., Aldecoa, R., & Delvenne, J.-C. (2015).
     Detecting communities using asymptotical surprise.
     Physical Review E, 92(2), 022816. 10.1103/PhysRevE.92.022816
 
+    :param initial_membership:
+    :param weights:
+    :param node_sizes:
     :param g:
     :return:
     """
 
     g = convert_graph_formats(g, ig.Graph)
 
-    part = leidenalg.find_partition(g, leidenalg.SurpriseVertexPartition)
+    part = leidenalg.find_partition(g, leidenalg.SurpriseVertexPartition, initial_membership=initial_membership,
+                                    weights=weights, node_sizes=node_sizes)
     coms = [g.vs[x]['name'] for x in part]
     return coms
 
