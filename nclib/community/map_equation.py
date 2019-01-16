0edead66c72ad523946a4d55e16e442f28283949
Unknown <milli@di.unipi.it>
2019-01-16 15:22:48 +0100

    add documentation map_equation

--- nclib/community/map_equation.py
+++ nclib/community/map_equation.py
@@ -9,9 +9,22 @@ from nclib.utils import convert_graph_formats
 
 def infomap(g):
     """
+    Infomap is based on ideas of information theory.
+    The algorithm uses the probability flow of random walks on a network as a proxy for information flows in the real system and it decomposes the network into modules by compressing a description of the probability flow.
 
-    :param g:
-    :return:
+    :param g: the graph
+    :return: a list of communities
+
+    :Example:
+
+    >>> from nclib import community
+    >>> import networkx as nx
+    >>> G = nx.karate_club_graph()
+    >>> coms = community.infomap(G)
+
+    :References:
+
+    Rosvall M, Bergstrom CT (2008) Maps of random walks on complex networks reveal community structure. Proc Natl Acad SciUSA 105(4):1118–1123
     """
     g = convert_graph_formats(g, nx.Graph)
 
@@ -39,9 +52,22 @@ def infomap(g):
 
 def walktrap(g):
     """
+    walktrap is an approach based on random walks.
+    The general idea is that if you perform random walks on the graph, then the walks are more likely to stay within the same community because there are only a few edges that lead outside a given community. Walktrap runs short random walks and uses the results of these random walks to merge separate communities in a bottom-up manner.
 
-    :param g:
-    :return:
+    :param g: the graph
+    :return: a list of communities
+
+    :Example:
+
+    >>> from nclib import community
+    >>> import networkx as nx
+    >>> G = nx.karate_club_graph()
+    >>> coms = community.walktrap(G)
+
+    :References:
+
+    Pons, Pascal, and Matthieu Latapy. "Computing communities in large networks using random walks." J. Graph Algorithms Appl. 10.2 (2006): 191-218.
     """
     g = convert_graph_formats(g, ig.Graph)
     coms = g.community_walktrap().as_clustering()
