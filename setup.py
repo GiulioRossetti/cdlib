f7e4618bf724104a04b0385c40519efc457f9c90
giuliorossetti <giulio.rossetti@gmail.com>
2019-01-30 10:02:47 +0100

    :arrow_up: requirements

--- setup.py
+++ setup.py
@@ -6,8 +6,55 @@ __author__ = 'Giulio Rossetti'
 __license__ = "BSD-2-Clause"
 __email__ = "giulio.rossetti@gmail.com"
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+def get_requirements(remove_links=True):
+    """
+    lists the requirements to install.
+    """
+
+    try:
+        with open('requirements.txt') as f:
+            requirements = f.read().splitlines()
+    except Exception as ex:
+        with open('DecoraterBotUtils.egg-info\requires.txt') as f:
+            requirements = f.read().splitlines()
+    if remove_links:
+        for requirement in requirements:
+            # git repository url.
+            if requirement.startswith("git+"):
+                requirements.remove(requirement)
+            # subversion repository url.
+            if requirement.startswith("svn+"):
+                requirements.remove(requirement)
+            # mercurial repository url.
+            if requirement.startswith("hg+"):
+                requirements.remove(requirement)
+    return requirements
+
+
+def get_links():
+    """
+    gets URL Dependency links.
+    """
+    links_list = get_requirements(remove_links=False)
+    for link in links_list:
+        keep_link = False
+        already_removed = False
+        # git repository url.
+        if not link.startswith("git+"):
+            if not link.startswith("svn+"):
+                if not link.startswith("hg+"):
+                    links_list.remove(link)
+                    already_removed = True
+                else:
+                    keep_link = True
+                if not keep_link and not already_removed:
+                    links_list.remove(link)
+                    already_removed = True
+            else:
+                keep_link = True
+            if not keep_link and not already_removed:
+                links_list.remove(link)
+    return links_list
 
 here = path.abspath(path.dirname(__file__))
 
@@ -46,6 +93,7 @@ setup(name='cdlib',
           'Programming Language :: Python :: 3'
       ],
       keywords='community-discovery node-clustering edge-clustering complex-networks',
-      install_requires=requirements,
+      install_requires=get_requirements(),
+      dependency_links=get_links(),
       packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test", "cdlib.test", "cdlib.test.*"]),
       )
