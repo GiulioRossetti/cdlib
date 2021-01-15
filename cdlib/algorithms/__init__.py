a8c7c618a50e51d1df44bd5e47e0e54a8620cd3a
Giulio Rossetti <giulio.rossetti@gmail.com>
2021-01-15 14:08:31 +0100

    :arrow_up: module import

--- cdlib/algorithms/__init__.py
+++ cdlib/algorithms/__init__.py
@@ -2,5 +2,5 @@ from .edge_clustering import *
 from .crisp_partition import *
 from .overlapping_partition import *
 from .attribute_clustering import *
-from .biparitte_clustering import *
+from .bipartite_clustering import *
 from .temporal_partition import *
