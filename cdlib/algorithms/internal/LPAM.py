a57c02f28708aed3f925a2e5ae851d8656c59036
Alexander Ponomarenko <aponom84@gmail.com>
2021-05-05 16:06:13 +0300

    :art: apply pep 8 coding style

--- cdlib/algorithms/internal/LPAM.py
+++ cdlib/algorithms/internal/LPAM.py
@@ -14,7 +14,8 @@ from pyclustering.cluster.kmedoids import kmedoids
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from cdlib import NodeClustering
 
-def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
+
+def lpam(graph, k=2, threshold=0.5, distance="amp", seed=0):
     """
     Link Partitioning Around Medoids
 
@@ -22,7 +23,7 @@ def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
     :param k: merging threshold in [0,1], default 0.25
     :param threshold: merging threshold in [0,1], default 0.5
     :param distance: type of distance: "amp" - amplified commute distance, or
-    "cm" - commute distance, or distance matrix between all edges as numpy ndarray
+    "cm" - commute distance, or distance matrix between all edges as np ndarray
     :param seed: random seed for k-medoid heuristic
 
     :return: NodeClustering object
@@ -44,7 +45,7 @@ def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
         """
         verts = list(G.nodes)
         n = len(verts)
-        vol = nx.volume(G,verts)
+        vol = nx.volume(G, verts)
 
         # get adj matrix
         A = nx.adjacency_matrix(G)
@@ -53,16 +54,18 @@ def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
         # use NetworkX to get Laplacian
         L = nx.laplacian_matrix(G)
         L = L.todense()
-        Gamma = L + (1/n) * np.ones([n,n])
-        CM = np.zeros([n,n])
+        Gamma = L + (1/n) * np.ones([n, n])
+        CM = np.zeros([n, n])
 
         # get Moore-Penrose pseudo inverse
         Gamma_pinv = np.linalg.pinv(Gamma, rcond=1e-4)
         for i in range(n):
-            for j in range(i+1,n):
-                CM[i,j] = vol*(Gamma_pinv[i,i] + Gamma_pinv[j,j] - 2 * Gamma_pinv[i,j])
-                CM[j,i] = CM[i,j]
+            for j in range(i+1, n):
+                CM[i, j] = vol*(Gamma_pinv[i, i]
+                                 + Gamma_pinv[j, j] - 2*Gamma_pinv[i, j])
+                CM[j, i] = CM[i, j]
         return CM
+
     def getAmp(G):
         """
         Returns amplified commute distance matrix
@@ -77,28 +80,30 @@ def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
         # use NetworkX to get Laplacian
         L = nx.laplacian_matrix(G)
         L = L.todense()
-        Gamma = L + (1/n) * np.ones([n,n])
-        C_AMP = np.zeros([n,n])
+        Gamma = L + (1/n) * np.ones([n, n])
+        C_AMP = np.zeros([n, n])
 
         # get Moore-Penrose pseudo inverse
         Gamma_pinv = np.linalg.pinv(Gamma, rcond=1e-4)
         for i in range(n):
-            for j in range(i+1,n):
-                r_ij = Gamma_pinv[i,i] + Gamma_pinv[j,j] - 2 * Gamma_pinv[i,j] # resistance dist
+            for j in range(i+1, n):
+                r_ij = (Gamma_pinv[i, i]
+                        + Gamma_pinv[j, j]
+                        - 2*Gamma_pinv[i, j])  # resistance dist
                 d_i = G.degree(list(G.nodes())[i])
                 d_j = G.degree(list(G.nodes())[j])
-                if d_i !=0 and d_j !=0:
+                if d_i != 0 and d_j != 0:
                     s_ij = r_ij - (1 / d_i) - (1 / d_j)
-                    w_ij = A[i,j]
-                    w_ii = A[i,i]
-                    w_jj = A[j,j]
-                    u_ij = ( ( 2 * w_ij) / (d_i * d_j) ) - ( w_ii / (d_i**2) ) - ( w_jj / (d_j**2) )
-                    C_AMP[i,j] = s_ij + u_ij
-                    C_AMP[j,i] = s_ij + u_ij
-
+                    w_ij = A[i, j]
+                    w_ii = A[i, i]
+                    w_jj = A[j, j]
+                    u_ij = (((2 * w_ij) / (d_i * d_j))
+                             - (w_ii / (d_i**2)) - (w_jj / (d_j**2)))
+                    C_AMP[i, j] = s_ij + u_ij
+                    C_AMP[j, i] = s_ij + u_ij
                 else:
-                    C_AMP[i,j] =  np.NaN
-                    C_AMP[j,i] =  np.NaN
+                    C_AMP[i, j] = np.NaN
+                    C_AMP[j, i] = np.NaN
         return C_AMP
 
     line_graph = nx.line_graph(graph)
@@ -111,12 +116,12 @@ def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
         D = distance
     if D is None:
         raise 'Parameter distance should be "amp"/"cm" or numpy.ndarray'
-
-
     _n = len(line_graph.nodes())
     np.random.seed(0)
     initial_medoids = np.random.choice(_n, k, replace=False)
-    kmedoids_instance = kmedoids(D, initial_medoids, data_type='distance_matrix')
+    kmedoids_instance = kmedoids(D,
+                                 initial_medoids,
+                                 data_type='distance_matrix')
     # run cluster analysis and obtain results
     kmedoids_instance.process()
 
@@ -139,27 +144,33 @@ def lpam(graph, k=2, threshold=0.5, distance = "amp", seed=0):
     for v, l in final_clusters.items():
         degree = len(l)
         res = defaultdict(list)
-        for x in l: res[x].append(x)
+        for x in l:
+            res[x].append(x)
         covering = np.zeros(k)
         for c_i, l in res.items():
             covering[c_i] = len(l)/degree
 
         res_clusters[v] = covering
 
-    _res_clusters = [ [] for i in range(k) ]
+    _res_clusters = [[] for i in range(k)]
 
     for v, l in res_clusters.items():
         for i in range(k):
             if l[i] >= threshold:
                 _res_clusters[i].append(v)
 
-    return NodeClustering(communities=[c for c in _res_clusters if len(c)>0], graph=graph, method_name='lpam', overlap = True)
+    return NodeClustering(communities=[c for c in _res_clusters if len(c) > 0],
+                          graph=graph,
+                          method_name='lpam',
+                          overlap=True)
+
+
 def run_demo():
     """
     Finds the communities of the Zachary graph
     """
     G = nx.karate_club_graph()
-    coms = lpam(G, k=2, threshold=0.4, distance = "amp")
+    coms = lpam(G, k=2, threshold=0.4, distance="amp")
     print(coms.communities)
 
 if __name__ == '__main__':
