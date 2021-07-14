c492149118bf6c98e4c2b7c312dafd84ef47b4b5
Giulio Rossetti <giulio.rossetti@gmail.com>
2021-07-14 14:22:52 +0200

    :memo: docs validation

--- cdlib/evaluation/comparisonranking.py
+++ cdlib/evaluation/comparisonranking.py
@@ -19,8 +19,9 @@ post_hoc = namedtuple("post_hoc", "comparison z_value p_value adj_p_value")
 class ComparisonRanking(object):
     def __init__(self, partitions: list):
         """
+        Class that provides functionalities for the generation of Clustering objects w.r.t. a given set of fitness scores.
 
-        :param partitions:
+        :param partitions: a list of Clustering objects
         """
         self.partitions = partitions
         self.rankings = {}
@@ -29,9 +30,22 @@ class ComparisonRanking(object):
 
     def rank(self, comparison_function: Callable[[object, object], object]):
         """
+        Computes the specified comparison function to all the Clustering objects for which a ranking is required.
 
-        :param comparison_function:
-        :return:
+        :param scoring_function: a fitness function from cdlib.evaluation
+        :return: a tuple whose first element is the scoring_function name, while the second is a dictionary having as key the clustering name and as value the computed score.
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.kclique(g, 2)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> rk = evaluation.ComparisonRanking([coms, coms2, coms3])
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_LFK)
         """
 
         ranks = {}
@@ -61,8 +75,30 @@ class ComparisonRanking(object):
 
     def topsis(self) -> [list, None]:
         """
+        The Technique for Order of Preference by Similarity to Ideal Solution (TOPSIS) is a multi-criteria decision analysis method.
+        TOPSIS is based on the concept that the chosen alternative should have the shortest geometric distance from the positive ideal solution (PIS) and the longest geometric distance from the negative ideal solution (NIS).
 
-        :return:
+        :return: a tuple whose first element is the ranking dictionary assigning a TOPSIS score to each Clustering object, while the second is None (to maintain coherence with friedman_ranking).
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.kclique(g, 2)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> rk = evaluation.ComparisonRanking([coms, coms2, coms3])
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_LFK)
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_MGH)
+        >>> rk.rank(evaluation.omega)
+        >>> rnk, _ = rk.topsis()
+
+        :References:
+
+        1. Hwang, C.L.; Yoon, K. (1981). Multiple Attribute Decision Making: Methods and Applications. New York: Springer-Verlag.
+        2. Yoon, K. (1987). "A reconciliation among discrete compromise situations". Journal of the Operational Research Society. 38 (3): 277–286. doi:10.1057/jors.1987.44.
         """
         rp = {
             a: [] for score, value in self.rankings.items() for a, vals in value.items()
@@ -94,8 +130,32 @@ class ComparisonRanking(object):
 
     def friedman_ranking(self) -> [list, float]:
         """
+        Performs a Friedman ranking test.
+        Tests the hypothesis that in a set of k dependent samples groups (where k >= 2) at least two of the groups represent populations with different median values.
 
-        :return:
+
+        :return: a tuple whose first element is a dictionary assigning a rank to each Clustering object, while the second is the p-value associated to the ranking.
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.kclique(g, 2)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> rk = evaluation.ComparisonRanking([coms, coms2, coms3])
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_LFK)
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_MGH)
+        >>> rk.rank(evaluation.omega)
+        >>> rnk, p_value = rk.friedman_ranking()
+
+        :References:
+
+        1. M. Friedman, The use of ranks to avoid the assumption of normality implicit in the analysis of variance, Journal of the American Statistical Association 32 (1937) 674–701.
+        2. D.J. Sheskin, Handbook of parametric and nonparametric statistical procedures. crc Press, 2003, Test 25: The Friedman Two-Way Analysis of Variance by Ranks
+
         """
 
         rp = {
@@ -128,8 +188,30 @@ class ComparisonRanking(object):
 
     def bonferroni_post_hoc(self) -> list:
         """
+        Performs a Bonferroni-Dunn post-hoc test using the pivot quantities obtained by a ranking test.
+        Tests the hypothesis that the ranking of the control method (best ranked clustering) is different to each of the other methods.
 
-        :return:
+        :return: a list of named tuples reporting the pairwise statistical significant comparisons among the best ranked clustering and the others (in terms of z-value, p-value, adjusted-p-value)
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.kclique(g, 2)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> rk = evaluation.ComparisonRanking([coms, coms2, coms3])
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_LFK)
+        >>> rk.rank(evaluation.overlapping_normalized_mutual_information_MGH)
+        >>> rk.rank(evaluation.omega)
+        >>> rnk, p_value = rk.friedman_ranking()
+        >>> pc = rk.bonferroni_post_hoc()
+
+        :References:
+
+        O.J. Dunn, Multiple comparisons among means, Journal of the American Statistical Association 56 (1961) 52–64.
         """
 
         res = []
--- cdlib/evaluation/fitnessranking.py
+++ cdlib/evaluation/fitnessranking.py
@@ -21,9 +21,10 @@ post_hoc = namedtuple("post_hoc", "comparison z_value p_value adj_p_value")
 class FitnessRanking(object):
     def __init__(self, graph: nx.Graph, partitions: list):
         """
+        Class that provides functionalities for the generation of Clustering objects w.r.t. a given set of fitness scores.
 
-        :param graph:
-        :param partitions:
+        :param graph: a Networkx/iGraph object
+        :param partitions: a list of Clustering objects
         """
         self.partitions = partitions
         self.graph = graph
@@ -35,9 +36,23 @@ class FitnessRanking(object):
         self, scoring_function: Callable[[nx.Graph, object], object]
     ) -> [str, dict]:
         """
+        Computes the specified scoring function to all the Clustering objects for which a ranking is required.
 
-        :param scoring_function:
-        :return:
+        :param scoring_function: a fitness function from cdlib.evaluation
+        :return: a tuple whose first element is the scoring_function name, while the second is a dictionary having as key the clustering name and as value the computed score.
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.demon(g, 0.25)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> coms4 = algorithms.angel(g, 0.6)
+        >>> rk = evaluation.FitnessRanking(g, [coms2, coms, coms3, coms4])
+        >>> rk.rank(evaluation.fraction_over_median_degree)
         """
 
         ranks = {}
@@ -58,8 +73,34 @@ class FitnessRanking(object):
 
     def topsis(self) -> [list, None]:
         """
+        The Technique for Order of Preference by Similarity to Ideal Solution (TOPSIS) is a multi-criteria decision analysis method.
+        TOPSIS is based on the concept that the chosen alternative should have the shortest geometric distance from the positive ideal solution (PIS) and the longest geometric distance from the negative ideal solution (NIS).
 
-        :return:
+        :return: a tuple whose first element is the ranking dictionary assigning a TOPSIS score to each Clustering object, while the second is None (to maintain coherence with friedman_ranking).
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.demon(g, 0.25)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> coms4 = algorithms.angel(g, 0.6)
+        >>> rk = evaluation.FitnessRanking(g, [coms2, coms, coms3, coms4])
+        >>> rk.rank(evaluation.fraction_over_median_degree)
+        >>> rk.rank(evaluation.edges_inside)
+        >>> rk.rank(evaluation.cut_ratio)
+        >>> rk.rank(evaluation.erdos_renyi_modularity)
+        >>> rk.rank(evaluation.newman_girvan_modularity)
+        >>> rk.rank(evaluation.modularity_density)
+        >>> rnk, _ = rk.topsis()
+
+        :References:
+
+        1. Hwang, C.L.; Yoon, K. (1981). Multiple Attribute Decision Making: Methods and Applications. New York: Springer-Verlag.
+        2. Yoon, K. (1987). "A reconciliation among discrete compromise situations". Journal of the Operational Research Society. 38 (3): 277–286. doi:10.1057/jors.1987.44.
         """
         rp = {
             a: [] for score, value in self.rankings.items() for a, vals in value.items()
@@ -85,8 +126,35 @@ class FitnessRanking(object):
 
     def friedman_ranking(self) -> [list, float]:
         """
+        Performs a Friedman ranking test.
+        Tests the hypothesis that in a set of k dependent samples groups (where k >= 2) at least two of the groups represent populations with different median values.
 
-        :return:
+
+        :return: a tuple whose first element is a dictionary assigning a rank to each Clustering object, while the second is the p-value associated to the ranking.
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.demon(g, 0.25)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> coms4 = algorithms.angel(g, 0.6)
+        >>> rk = evaluation.FitnessRanking(g, [coms2, coms, coms3, coms4])
+        >>> rk.rank(evaluation.fraction_over_median_degree)
+        >>> rk.rank(evaluation.edges_inside)
+        >>> rk.rank(evaluation.cut_ratio)
+        >>> rk.rank(evaluation.erdos_renyi_modularity)
+        >>> rk.rank(evaluation.newman_girvan_modularity)
+        >>> rk.rank(evaluation.modularity_density)
+        >>> rnk, p_value = rk.friedman_ranking()
+
+        :References:
+
+        1. M. Friedman, The use of ranks to avoid the assumption of normality implicit in the analysis of variance, Journal of the American Statistical Association 32 (1937) 674–701.
+        2. D.J. Sheskin, Handbook of parametric and nonparametric statistical procedures. crc Press, 2003, Test 25: The Friedman Two-Way Analysis of Variance by Ranks
         """
 
         rp = {
@@ -114,8 +182,34 @@ class FitnessRanking(object):
 
     def bonferroni_post_hoc(self) -> list:
         """
+        Performs a Bonferroni-Dunn post-hoc test using the pivot quantities obtained by a ranking test.
+        Tests the hypothesis that the ranking of the control method (best ranked clustering) is different to each of the other methods.
 
-        :return:
+        :return: a list of named tuples reporting the pairwise statistical significant comparisons among the best ranked clustering and the others (in terms of z-value, p-value, adjusted-p-value)
+
+        :Example:
+
+        >>> import networkx as nx
+        >>> from cdlib import evaluation
+        >>> from cdlib import algorithms
+        >>> g = nx.karate_club_graph()
+        >>> coms = algorithms.louvain(g)
+        >>> coms2 = algorithms.demon(g, 0.25)
+        >>> coms3 = algorithms.label_propagation(g)
+        >>> coms4 = algorithms.angel(g, 0.6)
+        >>> rk = evaluation.FitnessRanking(g, [coms2, coms, coms3, coms4])
+        >>> rk.rank(evaluation.fraction_over_median_degree)
+        >>> rk.rank(evaluation.edges_inside)
+        >>> rk.rank(evaluation.cut_ratio)
+        >>> rk.rank(evaluation.erdos_renyi_modularity)
+        >>> rk.rank(evaluation.newman_girvan_modularity)
+        >>> rk.rank(evaluation.modularity_density)
+        >>> rnk, p_value = rk.friedman_ranking()
+        >>> pc = rk.bonferroni_post_hoc()
+
+        :References:
+
+        O.J. Dunn, Multiple comparisons among means, Journal of the American Statistical Association 56 (1961) 52–64.
         """
 
         res = []
