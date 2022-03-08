b473564a4804ad93e2496e91c60ba722549a9209
Giulio Rossetti <giulio.rossetti@gmail.com>
2022-03-08 14:08:08 +0100

    :arrow_up: v0.2.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 
 setup(name='cdlib',
-      version='0.2.5',
+      version='0.2.6',
       license='BSD-Clause-2',
       description='Community Discovery Library',
       url='https://github.com/GiulioRossetti/cdlib',
