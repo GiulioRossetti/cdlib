325821037cc738f48e701856edf67cb2f36954f6
GiulioRossetti <giulio.rossetti@gmail.com>
2024-05-14 14:17:49 +0200

    :memo: logo

--- docs/conf.py
+++ docs/conf.py
@@ -129,6 +129,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 #html_theme = 'alabaster'
 
+html_logo = "cdlib_new.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
